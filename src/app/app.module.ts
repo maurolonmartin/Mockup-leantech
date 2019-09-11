e3e92a01859ac9e76043745b8900a411192cbedc
maurolonmar8 <maurolonmartin@hotmail.com>
2019-09-11 15:29:53 -0500

    cargado inicial del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessagesComponent,
+    SettingsComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
